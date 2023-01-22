9a92ea6de7048f1845dcadde818d7c06a9c915fb
Sohaib Manah <souhaibemanah@gmail.com>
2023-01-22 22:56:05 +0100

    project intialisation

---
+++ models/users.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+const UsersSchema = new mongoose.Schema({
+  username: String,
+});
+module.exports = mongoose.model("users", UsersSchema);
---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const UsersController = require("../controllers/users"); //UsersController is the class (controller)
+
+/* GET users listing. */
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await UsersController.getUser(id);
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(500).json({
+      status: "fail",
+      message: e.message,
+    });
+  }
+});
+
+module.exports = router;
