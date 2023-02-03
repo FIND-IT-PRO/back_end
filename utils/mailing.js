5b2d2448be042d2d80e46cbd172b00de5b0cd76f
Sohaib Manah <souhaibemanah@gmail.com>
2023-02-03 16:17:21 +0100

    change the mail provider from mailtrap to azure communcation services

--- utils/mailing.js
+++ utils/mailing.js
@@ -1,31 +1,14 @@
-const nodemailer = require("nodemailer");
+const { EmailClient } = require("@azure/communication-email");
+require("dotenv").config();
 
-const sendEmail = async (options) => {
-  // 1) Create a transporter
-  let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    // secure: true,
-    // secureConnection: true,
-    // logger: true,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
+// This code demonstrates how to fetch your connection string
+// from an environment variable.
+const connectionString =
+  process.env["COMMUNICATION_SERVICES_CONNECTION_STRING"];
+if (!connectionString) {
+  throw new Error(
+    "COMMUNICATION_SERVICES_CONNECTION_STRING is not set in the environment. Please refer to README.md to set this variable."
+  );
+}
 
-    // Activate in gmail "less secure app" option
-  });
-
-  // 2) Define the email options
-  const mailOptions = {
-    from: "Anas ZENAGUI <user@gmail.com>",
-    to: options.email,
-    subject: options.subject,
-    text: options.text,
-  };
-
-  // 3) Actually send the email
-  await transporter.sendMail(mailOptions);
-};
-
-module.exports = sendEmail;
+module.exports = new EmailClient(connectionString);
